14cb4ff9d0f9232b511e1312a19348b8bffe13cd
Enrico Bertolacini <enricobbmelo@gmail.com>
2026-09-07 10:41:14 -0300

    feat: configura CORS para integração com frontend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import database
 
@@ -9,6 +10,14 @@ from controller.controller_cliente import router as cliente_router
 app = FastAPI(
     title="API de Biblioteca",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 
